0a94379a21c3db95cdbafd86f36bc743410ed8c1
younghu <2044124998@qq.com>
2024-01-13 12:29:12 +0800

    first commit

---
+++ src/main/java/com/young/shibin/ShibinApplication.java
@@ -0,0 +1,13 @@
+package com.young.shibin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShibinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShibinApplication.class, args);
+    }
+
+}
