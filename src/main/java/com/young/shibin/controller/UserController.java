3459b340cab4b0baa786817d9a6b3402e7e3b609
younghu <2044124998@qq.com>
2024-01-14 21:30:55 +0800

    数据库配置

--- src/main/java/com/young/shibin/controller/UserController.java
+++ src/main/java/com/young/shibin/controller/UserController.java
@@ -3,14 +3,18 @@ package com.young.shibin.controller;
 import com.young.shibin.pojo.Result;
 import com.young.shibin.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequestMapping("/user")
 @RestController
 public class UserController {
 
     @Autowired
     private UserService userService;
 
+    @PostMapping("/register")
     public Result register(String username, String password) {
          userService.register(username, password);
          return Result.success();
